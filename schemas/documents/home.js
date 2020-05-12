4e99df14a6391324547fe19f0b1df9de175b3200
Gilad Grant <ggrant966@gmail.com>
2020-05-12 15:50:55 +0100

    Add dedicated featured recipe content

--- schemas/documents/home.js
+++ schemas/documents/home.js
@@ -2,10 +2,11 @@ export default {
     title: 'Home',
     name: 'home',
     type: 'document',
-    initialValue: {
-        showFeaturedTopic: true
-    },
     fieldsets: [
+        {
+            title: 'Featured recipe',
+            name: 'featuredRecipe',
+        },
         {
             title: 'Featured topic',
             name: 'featuredTopic',
@@ -20,6 +21,25 @@ export default {
         }
     ],
     fields: [
+        {
+            title: 'Post',
+            name: 'featuredRecipe',
+            type: 'reference',
+            to: [{ type: 'post' }],
+            options: {
+                filter: 'defined(content[0].steps)'
+            },
+            validation: Rule => Rule.required().error('Add a featured post'),
+            fieldset: 'featuredRecipe'
+        },
+        {
+            title: 'Description',
+            name: 'featuredRecipeDescription',
+            type: 'text',
+            rows: 5,
+            validation: Rule => [Rule.required().max(160).error('Max 160 characters')],
+            fieldset: 'featuredRecipe'
+        },
         {
             title: 'Subtitle',
             name: 'featuredTopicSubtitle',
@@ -40,7 +60,7 @@ export default {
                 {
                     title: 'Post',
                     type: 'reference',
-                    to: [{type: 'post'},]
+                    to: [{ type: 'post' },]
                 },
             ],
             fieldset: 'featuredTopic',
