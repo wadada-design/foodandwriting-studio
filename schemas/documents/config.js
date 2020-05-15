4f93e08dedc16d6b6d20d3ea1a1d19cdf994eeab
Gilad Grant <ggrant966@gmail.com>
2020-05-15 15:04:57 +0100

    Update site config schema

--- schemas/documents/config.js
+++ schemas/documents/config.js
@@ -6,29 +6,58 @@ export default {
     type: 'document',
     fieldsets: [
         {
-            title: 'SEO',
-            name: 'seo',
+            title: 'Site config',
+            name: 'siteConfig'
+        },
+        {
+            title: 'Social',
+            name: 'social',
         }
     ],
     fields: [
         {
-            title: 'SEO Description',
-            name: 'seoDecsription',
+            title: 'Site title',
+            name: 'siteTitle',
+            type: 'string',
+            fieldset: 'siteConfig',
+            validation: Rule => Rule.required().error('Missing site title')
+        },
+        {
+            title: 'Title template',
+            name: 'titleTemplate',
+            type: 'string',
+            fieldset: 'siteConfig',
+            description: 'Template for page titles. %PAGETITLE% is required. You can also use %SITETITLE%',
+            validation: Rule => [
+                Rule.custom(str => str ? (str.includes('%PAGETITLE%') ? true : '%TITLE% missing from the format') : 'Required')
+            ]
+        },
+        {
+            title: 'Site Description',
+            name: 'siteDecsription',
             type: 'text',
-            fieldset: 'seo',
-            description: 'Max characters 160',
+            fieldset: 'siteConfig',
+            description: 'Max characters 160. You can use %SITETITLE%',
             rows: 2,
             validation: Rule => [
                 Rule.max(160).error('Too many characters')
             ]
         },
         {
+            title: 'Instagram handle',
+            name: 'instagramHandle',
+            type: 'string',
+            fieldset: 'social',
+            validation: Rule => Rule.required().error('Missing instagram handle')
+        },
+        {
             title: 'Social media image',
             name: 'socialMediaImage',
             type: 'image',
+            fieldset: 'social',
             description: 'Image used when sharing the site to social media (posts will use their featured image when shared)',
-            fieldset: 'seo'
         },
+
     ],
     preview: {
         select: {
